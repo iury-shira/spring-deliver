ab07cc26c048d8f6dbe505d38bef15fda574c9e4
iury_resend <iuryresende628@gmail.com>
2021-01-11 12:10:36 -0300

    Inserting the frontend part

---
+++ front-web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <Hello message="Musk"/>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;

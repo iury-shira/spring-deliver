0410961b6745f555ccb78a09929d380cb26fbb7e
iury_resend <iuryresende628@gmail.com>
2021-01-13 16:58:03 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import Hello from './Hello';
 import Counter from './Counter';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
